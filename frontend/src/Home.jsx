a81254fc05fe8cfeb2ad117a371b6667d59217ff
cristian emmanuel lorenzo <cristianelprof@gmail.com>
2023-10-10 23:39:18 -0300

    Revert "Fix de error con logo"
    This reverts commit 49e5efd404caa4412aa86fca32a32811e451de8d.

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom"; // Agrega esta importación
-
 export function Home() {
   return (
     <>
       <div>
-        <h1>Outlet Zona Deportiva</h1>
-        <h3>Depósito</h3>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Outlet Zona Deportiva</h1>
+      <h3>Depósito</h3>
       <div className="espacio-bajo-encabezado">
         <ul className="facebook-list">
           {/* <li><Link to="/">home</Link></li> */}
