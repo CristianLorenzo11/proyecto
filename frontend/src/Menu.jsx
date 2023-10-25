33da5031b7edab45f56efde6f7accd89a334917f
cristian emmanuel lorenzo <cristianelprof@gmail.com>
2023-10-25 12:28:04 -0300

    agregue el nav link
    para que cuando seleccionamos una opcion del menu se note diferente

--- frontend/src/Menu.jsx
+++ frontend/src/Menu.jsx
@@ -1,56 +1,106 @@
 // Menu.jsx
 import React, { useEffect, useState } from "react";
-import "./Menu.css"
-import { Link } from "react-router-dom";
+import "./Menu.css";
+import { NavLink } from "react-router-dom";
 
-export function Menu(){
-  const [usuario, setUsuario]= useState('')
-  
+export function Menu() {
+  const [usuario, setUsuario] = useState("");
+
   useEffect(() => {
-    const usuarioLogueado = JSON.parse(localStorage.getItem('usuario'))
+    const usuarioLogueado = JSON.parse(localStorage.getItem("usuario"));
     if (usuarioLogueado) {
       setUsuario(usuarioLogueado);
     } else {
-      window.location.href = '/';
+      window.location.href = "/";
     }
   }, []);
 
   const cerrarSesionHandler = () => {
     localStorage.clear();
-    window.location.href = '/login';
+    window.location.href = "/login";
   };
 
   return (
     <>
-    
-    <nav class="navbar navbar-expand-sm " aria-label="Third navbar example">
-                <div className="botones container-fluid">
-               
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                            <li className="nav-item">
-          <Link className="nav-link active" aria-current="page"   to="/principal">Inicio</Link>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/producto">Productos</Link>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/proveedor">Proveedores</Link>
-          </li>
-          <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="/marca">Marcas</Link>   </li>
-          <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="/presentacion">Presentación</Link> </li>
-          <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="/tipo_producto">Tipo de Productos</Link>    </li>
-         </ul>
-          <button className="cerrar" onClick={cerrarSesionHandler}>Cerrar Sesión</button>
+      <nav className="navbar navbar-expand-sm" aria-label="Third navbar example">
+        <div className="botones container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  to="/principal"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/producto"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Productos
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/proveedor"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Proveedores
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/marca"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Marcas
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/presentacion"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Presentación
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/tipo_producto"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Tipo de Productos
+                </NavLink>
+              </li>
+            </ul>
+            <button className="cerrar" onClick={cerrarSesionHandler}>
+              Cerrar Sesión
+            </button>
+          </div>
         </div>
-        
-    </div>
       </nav>
-      <div className="usuario-conectado" > <span class="nombre-usuario">Usuario: {usuario.user}</span></div>
-  
+      <div className="usuario-conectado">
+        <span className="nombre-usuario">Usuario: {usuario.user}</span>
+      </div>
     </>
   );
 }
